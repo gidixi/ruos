4073ad3fd25af405c89cb7d35d057952ebb614fa
g.desolda <g.desolda@finework.it>
2026-05-30 14:33:23 +0200

    chore(init): tidy pipe smoke line (ls /bin | wc -l)

--- user-bin/init.sh
+++ user-bin/init.sh
@@ -33,7 +33,5 @@ cp /etc/init.sh /mnt/init.bak
 cat /mnt/init.bak | head -n 3
 echo --- ping test ---
 ping -c 1 -W 2000 10.0.2.2
-echo --- PIPE TEST ---
+echo --- pipe smoke ---
 ls /bin | wc -l
-ls / | grep bin
-echo --- PIPE END ---
